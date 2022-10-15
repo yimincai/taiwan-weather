25198b7468f13d19678dff4b8261cb0c277fec72
Neil Cai <bravc29229@gmail.com>
2022-10-15 14:23:35 +0800

    [add] use VerticalLayout

--- src/main/java/yimincai/net/taiwanweather/views/main/MainView.java
+++ src/main/java/yimincai/net/taiwanweather/views/main/MainView.java
@@ -1,27 +1,33 @@
 package yimincai.net.taiwanweather.views.main;
 
 import com.jayway.jsonpath.JsonPath;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.beans.factory.annotation.Autowired;
 import yimincai.net.taiwanweather.Forecast;
 import yimincai.net.taiwanweather.service.CityService;
 import yimincai.net.taiwanweather.service.WeatherService;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@PageTitle("Main")
+@PageTitle("Weather APP")
 @Route(value = "")
-public class MainView extends HorizontalLayout {
+public class MainView extends VerticalLayout {
 
     @Autowired
     private WeatherService weatherService;
-    private final Select<String> select;
+    private final Grid<Forecast> grid = new Grid<>(Forecast.class, false);
+    private Select<String> select;
     private List<String> parameter;
     private List<String> startTime;
     private List<String> endTime;
@@ -29,13 +35,21 @@ public class MainView extends HorizontalLayout {
 
     public MainView() {
         setMargin(true);
+        setUpSelector();
+        setUpTable();
+    }
+
+
+    public void setUpSelector() {
         select = new Select<>();
         select.setLabel("City");
         List<String> cityList = CityService.getCityList();
         select.setItems(cityList);
         add(select);
+    }
 
-        Grid<Forecast> grid = new Grid<>(Forecast.class, false);
+    public void setUpTable() {
+        grid.setWidthFull();
         grid.addColumn(Forecast::getTime).setHeader("Time");
         grid.addColumn(Forecast::getWx).setHeader("Wx");
         grid.addColumn(Forecast::getPop).setHeader("PoP");
@@ -60,6 +74,7 @@ public class MainView extends HorizontalLayout {
                 parameter = JsonPath.read(jsonString, String.format("$.records.location[*].weatherElement[%d].time[*].parameter.parameterName", i));
                 parameterData.put(elementName.get(i), parameter);
             }
+
             // set grid data
             List<Forecast> forecastsList = new java.util.ArrayList<>(List.of());
             if (startTime != null) {
@@ -71,7 +86,6 @@ public class MainView extends HorizontalLayout {
                     List<String> ci = parameterData.get("CI");
                     forecastsList.add(new Forecast(startTime.get(i) + " ~ " + endTime.get(i), wx.get(i), pop.get(i) + "%", minT.get(i) + "°C", maxT.get(i) + "°C", ci.get(i)));
                 }
-
             }
             grid.setItems(forecastsList);
         });
